e962eb1f9074b965e00d2012a1e41036ed5bdf50
Benjamin Horn <hornbd96@gmail.com>
2017-04-13 13:07:55 -0400

    Update query 5

--- rdb2017project/query5.sql
+++ rdb2017project/query5.sql
@@ -2,6 +2,6 @@
 contains at least two products with a warranty length greater than 1. */
 SELECT sum(s.repair_amount), s.product_category_id, s.product_name
 FROM hornbd.sold_product s
-WHERE s.length_date > 1
+WHERE product_category_id in( SELECT product_category_id FROM hornbd.sold_product WHERE length_date > 1 GROUP BY product_category_id HAVING count(*) > 2)
 GROUP BY s.product_category_id, s.product_name
-HAVING count(*)> 2;
+ORDER BY s.product_category_id, s.product_name
